7cbb69942702d1695d6fa6d03960b9645856e465
doylefermi <doylefermi@gmail.com>
2016-01-24 15:43:24 +0530

    Added basic GPS stuff

--- TrackMyRider/app/src/main/java/com/ridealong/haxorware/trackmyrider/MainActivity.java
+++ TrackMyRider/app/src/main/java/com/ridealong/haxorware/trackmyrider/MainActivity.java
@@ -1,30 +1,81 @@
 package com.ridealong.haxorware.trackmyrider;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements LocationListener {
+    private LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 1, this);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            //Toast.makeText(getBaseContext(), "No permissions", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String msg = "Latitude: " + location.getLatitude()
+                + "\nLongitude: " + location.getLongitude();
+        textView.setText(msg);
+        //Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onProviderEnabled(String provider) {
+
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
