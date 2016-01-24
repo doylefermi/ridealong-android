d4a5cddd8e93f6d4bd4142b104fc0f092c9615ff
doylefermi <doylefermi@gmail.com>
2016-01-24 07:27:15 +0530

    Test server with username and password

---
+++ android_test_server/public_html/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'mysql1.000webhost.com');
+define('DB_USERNAME', 'a2084959_maindb');
+define('DB_PASSWORD', 'doylesawesome007');
+define('DB_DATABASE', 'a2084959_maindb');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
